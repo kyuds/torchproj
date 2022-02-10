f707e99cd1528068a3ab73faa2a04b1dbc67d679
Kyuseung Shin <kyuds1016@berkeley.edu>
2022-02-09 16:42:41 -0800

    .

--- starter/trainer.py
+++ starter/trainer.py
@@ -23,7 +23,7 @@ trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=batch_size, shuffle=True, num_workers=2
 )
 
-"""
+""" 
 testset = torchvision.datasets.CIFAR10(
     root="./data", train=False, download=True, transform=transform
 )
